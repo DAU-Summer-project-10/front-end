d2fd2edf75d0bd3484c7a33a3ef5e65a6e5f5e5d
JJonDoggy <132hyooo@gmail.com>
2025-07-22 23:51:18 +0900

    JS파일 변경 및 readme 수정
    + 폴더 추가

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # front-end
+> ## 프로젝트 10팀 하로롱 프론트 레포지토리 입니다.
+
+---
+
+    - React Native
+    - Android 14.0 (UpSideDownCake)
---
+++ harorong/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {
+	View,
+	Text,
+	StyleSheet,
+} from 'react-native'
+
+
+const App = () => {
+	return (
+		<View></View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: 'red'
+	},
+	test: {
+	
+	}
+})
+
+
+export default App
