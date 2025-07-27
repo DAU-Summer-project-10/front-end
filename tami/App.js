bdb0932173cdbcc5c46246655026d3eeb5e35c8f
JJonDoggy <132hyooo@gmail.com>
2025-07-27 12:52:20 +0900

    rename 프로젝트명 변경

---
+++ tami/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+	View,
+    Text,
+	StyleSheet,
+	ScrollView,
+    SafeAreaView
+} from 'react-native'
+
+
+
+const App = () => {
+	return (
+		<SafeAreaView style={styles.testContainer}>
+            <Text style={styles.testText}>테스트</Text>
+        </SafeAreaView>
+	)
+}
+
+const styles = StyleSheet.create({
+	testContainer: {
+		borderRadius: 10,
+		justifyContent: 'center',
+		margin: 20
+	},
+	testText: {
+		fontSize: 25,
+		marginVertical: 10,
+		marginHorizontal: 20,
+		color: 'black'
+	}
+})
+
+export default App
